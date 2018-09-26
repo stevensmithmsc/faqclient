da98c8bca8905ea539b3b8f9f7e530892365f309
stevensmithmsc <stevensmithmsc@gmail.com>
2018-09-26 14:30:28 +0100

    redux added

---
+++ src/actions/index.js
@@ -0,0 +1,32 @@
+
+
+export const GET_ALL_Qs = 'get_all_qs';
+export const GET_CATEGORY = 'get_category';
+export const SEARCH_KEYWORD = 'search_keyword';
+export const GET_ANSWER = 'get_answer';
+export const UPDATE_ANSWER = 'update_answer';
+export const CREATE_QUESTION = 'create_question';
+
+export function getAllQs() {
+
+}
+
+export function getCategory(category) {
+
+}
+
+export function searchKeyword(keyword) {
+
+}
+
+export function getAnswer(id) {
+
+}
+
+export function updateAnswer(id, question) {
+
+}
+
+export function createQuestion(question) {
+
+}
